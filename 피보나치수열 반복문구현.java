74daa93d4a10072314b0a6a703439ed6d30708ad
YoungGon KIM <33260476+zeroGone@users.noreply.github.com>
2018-06-09 21:20:04 +0900

    0609 피보나치수열 반복문으로 구현

---
+++ 피보나치수열 반복문구현.java
@@ -0,0 +1,21 @@
+﻿/* 피보나치 수열 구현
+ * 동적프로그래밍을 이용하여
+ * 반복문으로 구현
+ * */
+public class Main {
+	
+	public static void main(String[] args) {
+		System.out.println(fibonacci(10));
+	}
+	
+	static int[] array = new int[100];
+	
+	public static int fibonacci(int num) {
+		array[1]=array[2]=1;
+		for(int i=3; i<=num; i++) {
+			array[i]=array[i-1]+array[i-2];
+		}
+		
+		return array[num];
+	}
+}
