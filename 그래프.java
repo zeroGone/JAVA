a31ac00f7a180c44b5562b5ae184c17b52d9346f
YoungGon KIM <33260476+zeroGone@users.noreply.github.com>
2018-06-18 10:45:17 +0900

    0618 BFS 까지 구현
    by lsj

--- 그래프.java
+++ 그래프.java
@@ -1,9 +1,13 @@
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
 
 public class Main {
-	public static HashMap<Character, String> createGraph() {
-        HashMap<Character, String> map = new HashMap<>();
+	public static Map<Character, String> createGraph() {
+        Map<Character,String> map = new HashMap<>();
         map.put('A', "BFH");  // A 정점에 인접한 정점 목록 등록
         map.put('B', "ACEF"); // B 정점에 인접한 정점 목록 등록
         map.put('C', "ABDE");
@@ -13,13 +17,13 @@ public class Main {
         map.put('G', "FH");
         map.put('H', "AF");
         return map;
-
 	}
-	public static void DFS(HashMap 그래프, char 시작) {
+	
+	public static void DFS(Map 그래프, char 시작) {
 		DFS(그래프, 시작, new HashSet<Character>());
 	}
 	
-	public static void DFS(HashMap 그래프, char 시작, HashSet 방문) {
+	public static void DFS(Map 그래프, char 시작, HashSet 방문) {
 		방문.add(시작);
 		System.out.print(시작+" ");
 		String value =(String) 그래프.get(시작);
@@ -28,11 +32,30 @@ public class Main {
 				DFS(그래프,value.charAt(i),방문);
 			}
 		}
+	}
+	
+	public static void BFS(Map<Character,String> 그래프, char 시작) {
+		Set<Character> 방문한정점 = new HashSet<>();
+		Queue<Character> 방문할정점 = new LinkedList<Character>();
 		
+		방문한정점.add(시작);
+		방문할정점.add(시작);
+		while(!방문할정점.isEmpty()) {
+			char print = 방문할정점.remove();
+			System.out.print(print+" ");
+			for(char temp : 그래프.get(print).toCharArray()) {
+				if(!방문한정점.contains(temp)) {
+					방문한정점.add(temp);
+					방문할정점.add(temp);
+				}
+			}
+		}
 	}
 	
 	public static void main(String[] args) {
 		DFS(createGraph(), 'A');
+		System.out.println();
+		BFS(createGraph(), 'A');
 	}
 
 }
